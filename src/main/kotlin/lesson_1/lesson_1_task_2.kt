81e920a56dc937fb435205392b42e5d1e7057ce9
Sergei Safonov <susafonov@yandex.ru>
2022-12-31 00:27:21 +0100

    KS-1-2 var println

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main(){
+    val numberOfOrder: Int = 75
+    val thankYouText: String = "Благодарим вас за покупку"
+
+    println(numberOfOrder)
+    println(thankYouText)
+
+    var numberOfStaff: Int = 2000  //Количество работников интернет магазина
+
+   // println(numberOfStaff)
+
+    numberOfStaff = 1999   // Количество работников после увольнения одного сотрудника
+
+    println(numberOfStaff)
+}
