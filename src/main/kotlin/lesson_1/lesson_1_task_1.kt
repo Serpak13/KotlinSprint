ae4191b6ac9e4648ff5de53c643e916aed40be81
Sergei Safonov <susafonov@yandex.ru>
2022-12-30 02:47:48 +0100

    KS-1-1 val print string int

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -2,4 +2,8 @@ package lesson_1
 
 fun main() {
 // Задача номер 1
+    val numberOfOrder: Int = 75
+    val thankYouText: String = "Благодарим вас за покупку"
+    println(numberOfOrder)
+    println(thankYouText)
 }
