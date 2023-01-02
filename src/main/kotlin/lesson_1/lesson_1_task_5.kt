91032621a294a50e1dc9d6b7fb217d972c1ab1d2
Sergei Safonov <susafonov@yandex.ru>
2023-01-02 23:28:22 +0100

    KS-1-5 val % println wholeMinutes remainSeconds

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -2,11 +2,11 @@ package lesson_1
 
 fun main() {
 
-    val second:Short= 6480
+    val second: Short = 6480
     //переводим секунды в минуты 6480 / 60 = 108, без остатка
     // либо же 6480 % 60 - выдаёт ноль, т.е. остатка нет
     val wholeMinutes = 108 //Целые минуты
-  //  println("${second%60}") - Проверка на остаток
+    //  println("${second%60}") - Проверка на остаток
     val remainsSeconds = "00"
     println("Время, проведённое в космосе $wholeMinutes:$remainsSeconds")
 }
