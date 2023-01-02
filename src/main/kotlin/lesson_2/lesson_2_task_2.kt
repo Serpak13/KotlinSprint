d58f0f6ed514e086d867ac7df3604a410909e6fc
Sergei Safonov <susafonov@yandex.ru>
2023-01-03 00:18:18 +0100

    KS-2-2 val arithmetic

---
+++ src/main/kotlin/lesson_2/lesson_2_task_2.kt
@@ -0,0 +1,19 @@
+package lesson_2
+
+fun main() {
+    //Вводные данные
+    val workerConst = 50  // кол-во постоянных сотрудников
+    val payCheckConst = 30000 // ЗП постоянных сотрудников
+    val workerInterim = 30 // кол-во стажёров
+    val payCheckInterim = 20000 // ЗП стажёров
+
+    // Расчёты
+
+    val expensesConst = workerConst * payCheckConst //Затраты на зп постоянных сотрудников
+    val totalExpenditure = expensesConst + (workerInterim * payCheckInterim) //Общие затраты после прихода стажёров
+    val arithmeticAfter = (payCheckConst + payCheckInterim) / 2 //средняя зп после устройства стажёров
+
+    println("Расходы на выплату зарплаты постоянных сотрудников - $expensesConst у.е.")
+    println("Общие расходы по зарплате после прихода сотрудников - $totalExpenditure у.е.")
+    println("Средняя зарплата одного сотрудника после прихода стажёров - $arithmeticAfter у.е.")
+}
