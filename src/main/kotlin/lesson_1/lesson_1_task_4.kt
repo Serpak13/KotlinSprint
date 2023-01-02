3c5628b7ced020852993a02816c7779bed73bb30
Sergei Safonov <susafonov@yandex.ru>
2023-01-02 23:09:47 +0100

    KS-1-4 val Long Byte Float Short Double Int

--- src/main/kotlin/lesson_1/lesson_1_task_4.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_4.kt
@@ -1,12 +1,12 @@
 package lesson_1
 
 fun main() {
-    val length:Long = 40_868_600_000
-    val age:Byte = 27
-    val partOfDay:Float = 0.075F
-    val second:Short= 6480
-    val partOfYear:Double = 2.0547945205479453E-4
-    val apogee:Int = 327000
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075F
+    val second: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
 
     println("Расстояние, преодолённое Гагариным в космосе - $length мм")
     println("Возраст Гагарина на момент полёта - $age лет")
@@ -14,7 +14,6 @@ fun main() {
     println("Количество секунд, проведённых в космосе - $second сек")
     println("Часть года, проведённая в космосе - $partOfYear")
     println("Апогей орбиты Гагарина - $apogee метров")
-
 
 
 }
