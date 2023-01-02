0fe8e83c877755add4938e7f579d3a6cec8916a2
Sergei Safonov <susafonov@yandex.ru>
2023-01-02 13:19:36 +0100

    KS-1-3 var year hour minute println

---
+++ src/main/kotlin/lesson_1_task_3/lesson_1_task_3.kt
@@ -0,0 +1,16 @@
+package lesson_1_task_3
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println("Год полёта в космос первого человека - $year год.")
+    println("В $hour часов")
+    println("$minute минут")
+
+    hour = 10
+    minute = 55
+
+    println("Время посадки $hour:$minute")
+}
