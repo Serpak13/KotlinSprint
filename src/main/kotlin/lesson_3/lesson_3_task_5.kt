b1938563aa5404349b80686c7eb9028cb2132b84
Sergei Safonov <susafonov@yandex.ru>
2023-01-04 23:09:19 +0100

    KS-3-5

---
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -0,0 +1,18 @@
+package lesson_3
+
+
+
+
+fun main() {
+    val startMotion = "D2"
+    val finishMotion = "D4"
+    val numberMotion = "0"
+
+    println("$startMotion-$finishMotion;$numberMotion")
+
+    println(startMotion)
+    println(finishMotion)
+    println(numberMotion)
+
+
+}
