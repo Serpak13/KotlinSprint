26c61c1725c6087fc435e9237c3b4c4f7982deb9
Sergei Safonov <susafonov@yandex.ru>
2023-01-05 00:11:27 +0100

    KS-4-1 const val

---
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -0,0 +1,17 @@
+package lesson_4
+
+fun main() {
+
+
+    val resToday = 13 //резерв сегодня
+
+    // так как известно что 4 стола свободны , значит 9 зарезервировано
+    val resTomorrow = 9 // резерв завтра
+
+
+    val resultToday = (resToday < TABLES)
+    val resultTomorrow = (resTomorrow < TABLES)
+
+    println("Доступность столиков на сегодня: $resultToday,\n Доступность столиков на завтра: $resultTomorrow" )
+}
+const val TABLES = 13
