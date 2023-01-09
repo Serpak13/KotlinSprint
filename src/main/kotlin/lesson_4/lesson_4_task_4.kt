2bf6d176e0e8af18131e89a0a4bbfacc75dd249f
Sergei Safonov <susafonov@yandex.ru>
2023-01-09 16:05:52 +0100

    KS-4-4

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -4,17 +4,20 @@ package lesson_4
 fun main() {
     val dayOfHands = 1
     val dayOfLegs = 3
-    val dayOfBack = 5
-    val dayOfAbs = 7
-    val trainingDay = 2
+    val dayOfBack = 7
+    val dayOfAbs = 1
+    val trainingDay = 5  // какой день тренировки
 
     println(
         """
-        |Упражнение для рук:${dayOfHands == trainingDay}
-        |Упражнение для ног:${dayOfLegs == trainingDay}
-        |Упражнения для спины:${dayOfBack == trainingDay}
-        |Упражнения для пресса:${dayOfAbs == trainingDay}
+        Упражнение для рук:${(dayOfHands == trainingDay) || (trainingDay == 5)}
+        Упражнение для ног:${dayOfLegs == trainingDay}
+        Упражнения для спины:${dayOfBack == trainingDay}
+        Упражнения для пресса:${dayOfAbs == trainingDay || (trainingDay == 5)}
         """.trimIndent()
     )
 
 }
+
+//const val PAIN_GAIN = 1 //нечётный день
+//const val RELAX = 0 //чётный день
