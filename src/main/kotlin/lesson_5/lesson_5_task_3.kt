eccb54ddedcb22eb67061ca8c690723f3ea16ad0
Sergei Safonov <susafonov@yandex.ru>
2023-01-10 01:49:14 +0100

    KS-5-3 if else readLine.toInt

--- src/main/kotlin/lesson_5/lesson_5_task_3.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -1,16 +1,32 @@
 package lesson_5
 
 fun main() {
-    println("Добро пожаловать в игру! Для того, чтобы выиграть главный приз, необходимо угадать два числа от 1 до 100.\nВведите первое число")
-    val numOne = readLine()!!.toInt()
+    //ПЕРВЫЙ ВАРИАНТ
+    //println("Добро пожаловать в игру! Для того, чтобы выиграть главный приз, необходимо угадать два числа от 1 до 100.\nВведите первое число")
+    //val numOne = readLine()!!.toInt()
+   // println("Введите второе число")
+   // val numTwo = readLine()!!.toInt()
+   // if ((numOne == NUM_ONE && numTwo == NUM_TWO) || (numOne == NUM_TWO && numTwo == NUM_ONE)) {
+   //     println("Поздравляем! Вы выиграли главный приз!")
+   // } else if ((numOne !== NUM_ONE && numTwo == NUM_TWO) || (numOne == NUM_ONE && numTwo !== NUM_TWO) || (numOne == NUM_TWO && numTwo !== NUM_ONE) || (numOne !== NUM_TWO && numTwo == NUM_ONE)) {
+      //  println("Вы выиграли утешительный приз!")
+    //} else {
+       // println("Неудача! Крутите барабан!")
+    //}
+    //ВТОРОЙ ВАРИАНТ
+    println("Введите первое число")
+    val contOne = readLine()!!.toInt()
     println("Введите второе число")
-    val numTwo = readLine()!!.toInt()
-    if ((numOne == NUM_ONE && numTwo == NUM_TWO) || (numOne == NUM_TWO && numTwo == NUM_ONE)) {
+    val contTwo = readLine()!!.toInt()
+    val massive = listOf(contOne, contTwo)
+    if(massive.contains(NUM_ONE) && massive.contains(NUM_TWO)){
         println("Поздравляем! Вы выиграли главный приз!")
-    } else if ((numOne !== NUM_ONE && numTwo == NUM_TWO) || (numOne == NUM_ONE && numTwo !== NUM_TWO) || (numOne == NUM_TWO && numTwo !== NUM_ONE) || (numOne !== NUM_TWO && numTwo == NUM_ONE)) {
+    }
+    else if(massive.contains(NUM_ONE) || massive.contains(NUM_TWO)){
         println("Вы выиграли утешительный приз!")
-    } else {
-        println("Неудача! Крутите барабан!")
+    }
+    else{
+        println("Неудача! Крутите барабан")
     }
 }
 
