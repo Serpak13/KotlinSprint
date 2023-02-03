eef4951c6e8c846c705e9122964f62565dc73a75
Sergei Safonov <susafonov@yandex.ru>
2023-02-03 20:26:04 +0100

    KS-11-5

--- src/main/kotlin/lesson_11/lesson_11_task_5.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_5.kt
@@ -17,61 +17,61 @@ package lesson_11
 //В функции main() создай экземпляр Forum и продемонстрируй его работу, добавив двух пользователей и по два сообщения от каждого пользователя.
 
 fun main() {
- val forum = Forum()
- val arthur = forum.newUser("Arthur", "pass1", "arthur.dent@mail.ru")
- val marvin = forum.newUser("Marvin", "pass2", "marvin@mail.ru")
+    val forum = Forum()
+    val arthur = forum.newUser("Arthur", "pass1", "arthur.dent@mail.ru")
+    val marvin = forum.newUser("Marvin", "pass2", "marvin@mail.ru")
 
- forum.newMessage(arthur.id, "Hello, Marv!")
- forum.newMessage(marvin.id, "Hi, Arthur!")
- forum.newMessage(arthur.id, "It is ok?")
- forum.newMessage(marvin.id, "Don't panic")
+    forum.newMessage(arthur.id, "Hello, Marv!")
+    forum.newMessage(marvin.id, "Hi, Arthur!")
+    forum.newMessage(arthur.id, "It is ok?")
+    forum.newMessage(marvin.id, "Don't panic")
 
- forum.printThread()
+    forum.printThread()
 }
 
 class User(
- val id: Int,
- val userName: String,
- val password: String,
- val email: String,
+    val id: Int,
+    val userName: String,
+    val password: String,
+    val email: String,
 )
 
 class Message(
- val id: Int,
- val author: User,
- val text: String,
+    val id: Int,
+    val author: User,
+    val text: String,
 )
 
 class Forum(
- private var messageIdCounter: Int = 0,
- private var userIdCounter: Int = 0,
- private val authors: MutableList<User> = mutableListOf(),
- private val messages: MutableList<Message> = mutableListOf(),
+    private var messageIdCounter: Int = 0,
+    private var userIdCounter: Int = 0,
+    private val authors: MutableList<User> = mutableListOf(),
+    private val messages: MutableList<Message> = mutableListOf(),
 ) {
- fun newUser(username: String, password: String, email: String): User {
-  val user = User(
-   id = userIdCounter,
-   userName = username,
-   password = password,
-   email = email,
-  )
-  authors.add(user)
-  userIdCounter++
-  return user
- }
+    fun newUser(username: String, password: String, email: String): User {
+        val user = User(
+            id = userIdCounter,
+            userName = username,
+            password = password,
+            email = email,
+        )
+        authors.add(user)
+        userIdCounter++
+        return user
+    }
 
- fun newMessage(authorId: Int, text: String) {
-  val user = authors.find { it.id == authorId }
-   ?: throw IllegalStateException("Author with id ${authorId} not found on forum")
-  val message = Message(id = messageIdCounter, author = user, text = text)
-  messages.add(message)
-  messageIdCounter++
- }
+    fun newMessage(authorId: Int, text: String) {
+        val user = authors.find { it.id == authorId }
+            ?: throw IllegalStateException("Author with id ${authorId} not found on forum")
+        val message = Message(id = messageIdCounter, author = user, text = text)
+        messages.add(message)
+        messageIdCounter++
+    }
 
- fun printThread() {
-  messages.forEach {
-   println("${it.author.userName}: ${it.text}")
-  }
- }
+    fun printThread() {
+        messages.forEach {
+            println("${it.author.userName}: ${it.text}")
+        }
+    }
 }
 
