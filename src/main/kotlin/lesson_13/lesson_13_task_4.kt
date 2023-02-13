2064af848c17cddb60eec690980d6cdbe58256df
Sergei Safonov <susafonov@yandex.ru>
2023-02-13 17:47:40 +0100

    KS-13-4

--- src/main/kotlin/lesson_13/lesson_13_task_4.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_4.kt
@@ -12,11 +12,6 @@ package lesson_13
 // а затем печатать все записи в консоли в компактном виде.
 fun main() {
 
-    /* val contact1 = PhoneDirectory("Ростислав", null, "Reddit")
-    val contact2 = PhoneDirectory("Павел", "89996541234", null)
-    val contact3 = PhoneDirectory(null, "83825648912", "Rockstar")
-    val contacts: List<PhoneDirectory> = listOf(contact1, contact2, contact3) //список контактов */
-
     addingInformationToDirectory()
 }
 
@@ -24,30 +19,26 @@ fun addingInformationToDirectory() {
     var contactsListNew: List<PhoneDirectoryNew> = mutableListOf()
     do {
         println("Введите имя контакта")
-        val newName = readlnOrNull() ?: "Не указано"
+        val newName = readLine()?.takeIf { it.isNotEmpty() } ?: "Не указано"
         println("Введите номер телефона")
-        val newTelephoneNumber = readlnOrNull() ?: return
+        val newTelephoneNumber = readLine()?.takeIf { it.isNotEmpty() } ?: "Не указано"
         println("Введите название компании")
-        val newCompanyName = readlnOrNull() ?: "Не указано"
+        val newCompanyName = readLine()?.takeIf { it.isNotEmpty() } ?: "Не указано"
         val newContacts = PhoneDirectoryNew(nameContacts =  newName, telephoneNumber = newTelephoneNumber, nameCompany = newCompanyName)
         contactsListNew += newContacts
         println("Хотите добавить ещё один контакт? Введите \"да\" или \"нет\"")
         val answerUser = readLine()!!
     } while (answerUser == "да")
-    println(contactsListNew)
 
+    for(i in contactsListNew){
+        println("Имя контакта: ${i.nameContacts}")
+        println("Номер телефона: ${i.telephoneNumber}")
+        println("Название компании: ${i.nameCompany}")
+        println()
+    }
 }
 data class PhoneDirectoryNew(var nameContacts: String?, var telephoneNumber: String?, var nameCompany: String?) {
 }
 
-/*fun printCompanyInformation(contactsAll: List<PhoneDirectoryNew>) {
-    val value1 = contactsAll[0]
-    val value2 = contactsAll[1]
-    val value3 = contactsAll[2]
-    println("Имя: ${value1.nameContacts ?: "Не указано"}\nНомер: ${value1.telephoneNumber ?: "Не указано"}\nКомпания: ${value1.nameCompany ?: "Не указано"}")
-    println()
-    println("Имя: ${value2.nameContacts ?: "Не указано"}\nНомер: ${value2.telephoneNumber ?: "Не указано"}\nКомпания: ${value2.nameCompany ?: "Не указано"}")
-    println()
-    println("Имя: ${value3.nameContacts ?: "Не указано"}\nНомер: ${value3.telephoneNumber ?: "Не указано"}\nКомпания: ${value3.nameCompany ?: "Не указано"}")
-}*/
+
 
