c748a9eda14beb9b0a23de6261837e6acfa05dba
Sergei Safonov <susafonov@yandex.ru>
2023-02-28 19:41:22 +0100

    KS-15-1 abstract class and fun interface

--- src/main/kotlin/lesson_15/lesson_15_task_1.kt
+++ src/main/kotlin/lesson_15/lesson_15_task_1.kt
@@ -13,14 +13,17 @@ fun main() {
     val crucian1 = Fish("Карась")
     val gull1 = Bird("Чайка")
     val duck1 = Bird("Утка")
-    crucian1.movementMethod()
-    gull1.movementMethod()
-    duck1.movementMethod()
+    crucian1.movementMethodWater()
+    crucian1.flyMethod()
+    gull1.flyMethod()
+    gull1.movementMethodWater()
+    duck1.flyMethod()
+    duck1.movementMethodWater()
 
 
 }
 
-abstract class Animal: Movementable{
+abstract class Animal: MovementableWater, Flyable{
     abstract  val name: String //Название животного
 }
 
@@ -28,19 +31,31 @@ class Fish(
     override val name: String
 ): Animal() {
 
-    override fun movementMethod() {
+    override fun movementMethodWater() {
         println("$name передвигается в воде за счёт плавников и хваста")
+    }
+
+    override fun flyMethod() {
+        println("$name не летает")
     }
 }
 
 class Bird(
     override val name: String
 ): Animal(){
-    override fun movementMethod() {
-        println("$name передвигается по воздуху за счёт крыльев, по земле на лапах и сплавляетя по воде")
+    override fun movementMethodWater() {
+        println("Туловище $name немного сплюснуто. За счет этого птица способна удерживаться на плаву. Также птица отличается тем, что её кости – полые, легкие.")
+    }
+
+    override fun flyMethod() {
+        println("$name передвигается по воздуху за счёт крыльев")
     }
 }
 
-interface Movementable {
-    fun movementMethod()
+interface MovementableWater {
+    fun movementMethodWater()
+}
+
+interface Flyable{
+    fun flyMethod()
 }
