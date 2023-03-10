379b54965ee186f0f06398b6761749d0714720fa
Sergei Safonov <susafonov@yandex.ru>
2023-03-10 01:57:23 +0100

    KS-16-3 private

---
+++ src/main/kotlin/lesson_16/lesson_16_task_3.kt
@@ -0,0 +1,23 @@
+package lesson_16
+
+//Задача 3 к Уроку 16
+//
+//Создай класс, описывающий пользователя. У него должен быть логин и пароль.
+// Пароль не должен быть виден в теле главной функции, но должен быть способ проверить, что пароль введен верно.
+// Выведи информацию о релевантности пароля в консоль.
+fun main() {
+
+    val user1 = User("Johny")
+    user1.checkPasword()
+}
+
+class User(val loginUser: String){
+
+    private val passwordUser = "qwerty"
+
+    fun checkPasword(){
+        println("Введите пароль")
+        if(readLine() == passwordUser) println("Пароль введён верно")
+        else println("Пароль введён неверно")
+    }
+}
