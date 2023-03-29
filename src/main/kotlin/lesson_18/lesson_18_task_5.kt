38f0584e06f5223a15817df23307cac1af58c3f5
Sergei Safonov <susafonov@yandex.ru>
2023-03-29 18:46:05 +0200

    KS-18-5

--- src/main/kotlin/lesson_18/lesson_18_task_5.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_5.kt
@@ -7,7 +7,12 @@ package lesson_18
 //
 //Реализуй классы точки, квадрата и круга, а также класс Screen со всеми перегруженными методами draw(...).
 fun main() {
-
+    val circle: Screen = Circle()
+    val square: Screen = Square()
+    val dot: Screen = Dot()
+    circle.drawObject(Circle(), 5.5f,6.6f)
+    square.drawObject(Square(), 9.5f,6.1f)
+    dot.drawObject(Dot(), 1.5f,5.6f)
 }
 
 open class Screen(){
@@ -15,17 +20,23 @@ open class Screen(){
         typeOfGeometricObject: Circle,
         coordinateX: Float,
         coordinateY: Float,
-    ){}
+    ){
+        println("Тип фигуры: $typeOfGeometricObject\nКоордината Х: $coordinateX\nКоордината Y: $coordinateY")
+    }
     open fun drawObject(
         typeOfGeometricObject: Square,
         coordinateX: Float,
         coordinateY: Float,
-    ){}
+    ){
+        println("Тип фигуры: $typeOfGeometricObject\nКоордината Х: $coordinateX\nКоордината Y: $coordinateY")
+    }
     open fun drawObject(
         typeOfGeometricObject: Dot,
         coordinateX: Float,
         coordinateY: Float,
-    ){}
+    ){
+        println("Тип фигуры: $typeOfGeometricObject\nКоордината Х: $coordinateX\nКоордината Y: $coordinateY")
+    }
 }
 
 class Circle: Screen(){}
