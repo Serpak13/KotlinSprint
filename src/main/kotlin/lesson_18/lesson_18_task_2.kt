26acf434b93f5a1deca5be972c7ae10489778444
Sergei Safonov <susafonov@yandex.ru>
2023-03-29 11:22:47 +0200

    KS-18-2

--- src/main/kotlin/lesson_18/lesson_18_task_2.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_2.kt
@@ -6,39 +6,37 @@ package lesson_18
 //В игре используются игральные кости с разным количеством граней: 4, 6 и 8.
 // Опиши их, используя полиморфизм, создав несколько классов. У каждой кости должен быть метод, печатающий в консоль выпавшее число.
 fun main() {
-    val cube1: Dice = Tetrahedron()
-    val cube2: Dice = Hexagon()
-    val cube3: Dice = Octahedron()
-    cube1.trowCube()
+    val dice1: Dice = Tetrahedron()
+    val dice2: Dice = Hexagon()
+    val dice3: Dice = Octahedron()
+    dice1.throwDice()
     println()
-    cube2.trowCube()
+    dice2.throwDice()
     println()
-    cube3.trowCube()
+    dice3.throwDice()
 
 }
 
 abstract class Dice(val  faces: Int) {
-    abstract fun trowCube()
+    protected val possibleNumbers = (1..faces)
+    abstract fun throwDice()
 }
 
 class Tetrahedron: Dice(4) {
-    override fun trowCube() {
-        val number = (1..faces).random()
-        println("Бросок кубика с количеством граней $faces. Выпавшее число - $number")
+    override fun throwDice() {
+        println("Бросок кубика с количеством граней $faces. Выпавшее число - ${possibleNumbers.random()}")
     }
 }
 
 class Hexagon: Dice(6) {
-    override fun trowCube() {
-        val number = (1..faces).random()
-        println("Бросок кубика с количеством граней $faces. Выпавшее число - $number")
+    override fun throwDice() {
+        println("Бросок кубика с количеством граней $faces. Выпавшее число - ${possibleNumbers.random()}")
     }
 }
 
 class Octahedron: Dice(8) {
-    override fun trowCube() {
-        val number = (1..faces).random()
-        println("Бросок кубика с количеством граней $faces. Выпавшее число - $number")
+    override fun throwDice() {
+        println("Бросок кубика с количеством граней $faces. Выпавшее число - ${possibleNumbers.random()}")
     }
 }
 
