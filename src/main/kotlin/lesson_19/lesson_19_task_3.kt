682968143a60a793e558d7f837663ee0ff83cde7
Sergei Safonov <susafonov@yandex.ru>
2023-03-30 18:38:29 +0200

    KS-19-3 TODO

---
+++ src/main/kotlin/lesson_19/lesson_19_task_3.kt
@@ -0,0 +1,31 @@
+package lesson_19
+
+
+//Задача 3 к Уроку 19
+//
+//В игре есть космический корабль. Он может взлетать, приземляться и отстреливаться от астероида.
+//
+//Задача описать класс и его методы. Требуемая реализация методов нам пока неизвестна.
+// Один из методов должен содержать явную пометку-напоминание о том, что здесь нужна информация о дополнительной логике.
+// Другой метод должен вызывать падение программы с ошибкой NotImplementedError.
+fun main() {
+    val ship1 = SpaceShip("Баракуда", 200, true)
+    ship1.actionFlayShip()
+    ship1.landingShip()
+    ship1.fireWeapon() // NotImplementedError.
+}
+
+class SpaceShip(val nameShip: String, val speedShip: Int, val presenceOfWeapons: Boolean){
+
+    fun actionFlayShip(){
+        println("Корабль взлетел")
+    }
+
+    fun landingShip(){
+        TODO("Необходимо прописать дополнительную логику")
+    }
+
+    fun fireWeapon(){
+        TODO()
+    }
+}
