fd3c52aa2ffdaa349a42580979a867a4ebbfd1e3
Sergei Safonov <susafonov@yandex.ru>
2023-03-31 00:29:13 +0200

    KS-19-2

--- src/main/kotlin/lesson_19/lesson_19_task_2.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_2.kt
@@ -11,27 +11,23 @@ package lesson_19
 // – для вывода информации о товаре, для категории используй метод определения категории.
 fun main() {
 
-    val product1: ProductDescription = ProductDescription("Свитшот", 1564, Category.CLOTHES)
-    val product2: ProductDescription = ProductDescription("Степлер", 5896, Category.CHANCELLERY)
-    val product3: ProductDescription = ProductDescription("Мочалка", 8526, Category.OTHER)
+    val product1 = ProductDescription("Свитшот", 1564, Category.CLOTHES)
+    val product2 = ProductDescription("Степлер", 5896, Category.CHANCELLERY)
+    val product3 = ProductDescription("Мочалка", 8526, Category.OTHER)
 
     product1.getInfoAboutProduct()
     println()
     product2.getInfoAboutProduct()
     println()
     product3.getInfoAboutProduct()
-
-
-
-
 }
 
 data class ProductDescription(val nameProduct: String, val id: Int, val category: Category) {
 
-    private val catProduct =  getNameCategory(category)
-     fun getInfoAboutProduct(){
-         println("Название товара:$nameProduct\nАртикул товара:$id\nКатегория товара:$catProduct")
-     }
+    private val catProduct = getNameCategory(category)
+    fun getInfoAboutProduct() {
+        println("Название товара:$nameProduct\nАртикул товара:$id\nКатегория товара:$catProduct")
+    }
 }
 
 enum class Category {
