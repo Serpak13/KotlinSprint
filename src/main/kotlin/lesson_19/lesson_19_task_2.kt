9472dd97d959ac0a622401096caaa1d65e4016f5
Sergei Safonov <susafonov@yandex.ru>
2023-03-30 18:02:13 +0200

    KS-19-2 ENUM

---
+++ src/main/kotlin/lesson_19/lesson_19_task_2.kt
@@ -0,0 +1,48 @@
+package lesson_19
+
+//Задача 2 к Уроку 19
+//
+//В интернет-магазине товары делятся на несколько категорий: одежда, канцелярские товары и разное.
+//
+// – создай enum класс с категориями;
+// – создай метод, который примет тип категории и вернет текстовое название категории для пользователя;
+// – создай класс, описывающий товар (с полями название, id, категория), в классе должен быть метод, который выводит
+//  инфо о товаре. Вызови его для нескольких созданных товаров;
+// – для вывода информации о товаре, для категории используй метод определения категории.
+fun main() {
+
+    val product1: ProductDescription = ProductDescription("Свитшот", 1564, Category.CLOTHES)
+    val product2: ProductDescription = ProductDescription("Степлер", 5896, Category.CHANCELLERY)
+    val product3: ProductDescription = ProductDescription("Мочалка", 8526, Category.OTHER)
+
+    product1.getInfoAboutProduct()
+    println()
+    product2.getInfoAboutProduct()
+    println()
+    product3.getInfoAboutProduct()
+
+
+
+
+}
+
+data class ProductDescription(val nameProduct: String, val id: Int, val category: Category) {
+
+    private val catProduct =  getNameCategory(category)
+     fun getInfoAboutProduct(){
+         println("Название товара:$nameProduct\nАртикул товара:$id\nКатегория товара:$catProduct")
+     }
+}
+
+enum class Category {
+    CLOTHES,
+    CHANCELLERY,
+    OTHER;
+}
+fun getNameCategory(category: Category) {
+    when (category) {
+        Category.CLOTHES -> println("Одежда")
+        Category.CHANCELLERY -> println("Канцелярия")
+        Category.OTHER -> println("Другое")
+    }
+}
