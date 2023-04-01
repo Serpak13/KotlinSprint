7d0316463a094aad761fdba053d965c79b4dbba0
Sergei Safonov <susafonov@yandex.ru>
2023-04-01 23:20:12 +0200

    KS-19-5

---
+++ src/main/kotlin/lesson_19/lesson_19_task_5.kt
@@ -0,0 +1,39 @@
+package lesson_19
+
+
+//Задача 5 к Уроку 19
+//
+//Разрабатываем приложение “Картотека”. Пользователь вводит имя и пол человека. Данные картотеки хранятся в списке.
+//
+//Человека описывает класс, содержащий имя и пол. Пол должен храниться в enum. После ввода 5 человек, список выводится в консоль.
+//
+//При старте приложения печатается инструкция в каком формате вводить имя и пол (и какие значения полов доступны).
+fun main() {
+
+    val listPeople: MutableList<Human> = mutableListOf<Human>()
+
+    for (i in 1..5) {
+        println("Введите имя")
+        val inputName: String = readlnOrNull().toString()
+        println("Введите пол \"М\" или \"Ж\"")
+        val inputGender = when (readLine()) {
+            "М" -> Gender.MAN
+            "Ж" -> Gender.FEMALE
+            else -> Gender.ERROR
+        }
+        listPeople.add(Human(name = inputName, gender = inputGender))
+    }
+    println(listPeople)
+}
+
+enum class Gender(nameGender: String) {
+    MAN("мужской"),
+    FEMALE("женский"),
+    ERROR("Ошибка ввода данных");
+
+}
+
+data class Human(val name: String, val gender: Gender) {
+
+}
+
